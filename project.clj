2a1e3ab2fe7d89200985d118efc243c58398cdd8
Geoff Shannon <geoffpshannon@gmail.com>
2018-07-02 02:34:20 -0700

    Update test dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject matrix-test "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [net.mikera/core.matrix "0.37.0"]
-                 [net.mikera/vectorz-clj "0.33.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.mikera/core.matrix "0.62.0"]
+                 [net.mikera/vectorz-clj "0.47.0"]])
